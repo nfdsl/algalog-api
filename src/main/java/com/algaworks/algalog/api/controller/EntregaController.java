82a549d8ec61ff61738bae861cef6c996897e307
Natan <nfsl@cin.ufpe.br>
2023-03-29 08:59:33 -0300

    Implementação Validação Cascata/Validation Groups

--- src/main/java/com/algaworks/algalog/api/controller/EntregaController.java
+++ src/main/java/com/algaworks/algalog/api/controller/EntregaController.java
@@ -1,21 +1,39 @@
 package com.algaworks.algalog.api.controller;
 
 import com.algaworks.algalog.domain.model.Entrega;
+import com.algaworks.algalog.domain.repository.EntregaRepository;
 import com.algaworks.algalog.domain.service.SolicitacaoEntregaService;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import org.hibernate.annotations.IdGeneratorType;
 import org.hibernate.validator.constraints.pl.REGON;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @AllArgsConstructor
 @RestController
 @RequestMapping("/entregas")
 public class EntregaController {
+    private EntregaRepository entregaRepository;
     private SolicitacaoEntregaService solicitacaoEntregaService;
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Entrega solicitar(@RequestBody Entrega entrega){
+    public Entrega solicitar( @Valid @RequestBody Entrega entrega){
         return solicitacaoEntregaService.solicitar(entrega);
     }
+
+    @GetMapping
+    public List<Entrega> listar(){
+        return entregaRepository.findAll();
+    }
+
+    @GetMapping("/{entregaId}")
+    public ResponseEntity<Entrega> buscar(@PathVariable Long entregaId){
+        return entregaRepository.findById(entregaId).map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+    }
+
 }
--- src/main/java/com/algaworks/algalog/domain/model/Entrega.java
+++ src/main/java/com/algaworks/algalog/domain/model/Entrega.java
@@ -1,11 +1,15 @@
 package com.algaworks.algalog.domain.model;
 
+import com.algaworks.algalog.domain.ValidationGroups;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.groups.ConvertGroup;
+import jakarta.validation.groups.Default;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.cglib.core.Local;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -20,16 +24,27 @@ public class Entrega {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long Id;
+
+    @Valid
+    @ConvertGroup(from = Default.class, to = ValidationGroups.ClienteId.class)
+    @NotNull
     @ManyToOne
     private Cliente cliente;
+
+    @Valid
+    @NotNull
     @Embedded
     private Destinatario destinatario;
+    @NotNull
     private BigDecimal taxa;
+
     @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     @Enumerated(EnumType.STRING)
     private StatusEntrega status;
+
     @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private LocalDateTime dataPedido;
+
     @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private LocalDateTime dataFinalizacao;
 
